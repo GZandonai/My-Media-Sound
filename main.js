a2ccb8990e5cdd86b3fadeeb41fd66c2271bdac2
Giovanni Zandonai <giovannizandonai350@gmail.com>
2024-07-11 15:39:52 -0300

    realizando a logica no main.js

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-
-
-function tocaSomPom (){
-    
-    document.querySelector('#som_tecla_pom').play()
-    
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla')
-
-listaDeTeclas[0].onclick = tocaSomPom
+function tocaSom(idAudio) {
+    const audioElement = document.querySelector(idAudio)
+    const newAudioElement = audioElement.cloneNode() // Clona o elemento de áudio
+    newAudioElement.play(); // Toca o novo elemento
+  }
+  
+  const listaDeTeclas = document.querySelectorAll(".tecla")
+  
+  let contador = 0
+  
+  while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador]
+  
+    const instrumento = tecla.classList[1]
+  
+    const idAudio = `#som_${instrumento}`
+  
+    tecla.onclick = function () {
+      tocaSom(idAudio);
+    };
+  
+    contador = contador + 1;
+  }
